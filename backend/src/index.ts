bb47b4aa92b5330a74b7d89652adbc98c4b06fc8
Guilherme Morone <guimorone@gmail.com>
2024-02-22 17:26:44 -0300

    teste com dispositivo

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,45 +24,72 @@ const serviceAccountAuth = new JWT({
 
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID || '', serviceAccountAuth);
 
-app.get('/spreadsheet', async (_req: Request, res: Response) => {
-  await doc.loadInfo();
+app
+  .route('/spreadsheet')
+  .get(async (_req: Request, res: Response) => {
+    try {
+      await doc.loadInfo();
 
-  const sheet = doc.sheetsByTitle['polls'];
-  const rows = await sheet.getRows();
-  const data: IPolls[] = [];
-  rows.forEach(row => {
-    data.push({
-      name: row.get('name') || undefined,
-      poll: row.get('poll'),
-      coords: [parseFloat(row.get('lat').replaceAll(',', '.')), parseFloat(row.get('lng').replaceAll(',', '.'))],
-      address: row.get('address'),
-      option1: row.get('option1'),
-      option2: row.get('option2'),
-      votes1: parseInt(row.get('votes1')) || 0,
-      votes2: parseInt(row.get('votes2')) || 0,
-    });
+      const sheet = doc.sheetsByTitle['polls'];
+      const rows = await sheet.getRows();
+      const data: IPolls[] = [];
+      rows.forEach(row => {
+        data.push({
+          name: row.get('name') || undefined,
+          poll: row.get('poll'),
+          coords: [
+            parseFloat(row.get('lat').replaceAll(',', '.')),
+            parseFloat(row.get('lng').replaceAll(',', '.')),
+          ],
+          address: row.get('address'),
+          option1: row.get('option1'),
+          option2: row.get('option2'),
+          votes1: parseInt(row.get('votes1')) || 0,
+          votes2: parseInt(row.get('votes2')) || 0,
+        });
+      });
+
+      res.status(200).send(data);
+    } catch (error: unknown) {
+      res.status(500).send(error);
+    }
+  })
+  .put((req: Request, res: Response) => {
+    try {
+      console.log(req);
+      console.log(req.body);
+
+      res.status(200).send(req.body);
+    } catch (error: unknown) {
+      res.status(500).send(error);
+    }
   });
 
-  res.status(200).send(data);
-});
-
 app.post('/suggestion', async (req: Request, res: Response) => {
-  await doc.loadInfo();
+  try {
+    await doc.loadInfo();
 
-  const sheet = doc.sheetsByTitle['suggestions'];
-  await sheet.addRow(req.body);
+    const sheet = doc.sheetsByTitle['suggestions'];
+    await sheet.addRow(req.body);
 
-  res.status(201).send('Sucesso!');
+    res.status(201).send('Sucesso!');
+  } catch (error: unknown) {
+    res.status(500).send(error);
+  }
 });
 
 app.post('/feedback', async (req: Request, res: Response) => {
-  await doc.loadInfo();
+  try {
+    await doc.loadInfo();
 
-  const sheet = doc.sheetsByTitle['feedbacks'];
-  req.body.score = parseFeedbackScore(req.body.score);
-  await sheet.addRow(req.body);
+    const sheet = doc.sheetsByTitle['feedbacks'];
+    req.body.score = parseFeedbackScore(req.body.score);
+    await sheet.addRow(req.body);
 
-  res.status(201).send('Sucesso!');
+    res.status(201).send('Sucesso!');
+  } catch (error: unknown) {
+    res.status(500).send(error);
+  }
 });
 
 app.listen(port, () => console.log(`[server]: Server is running on port ${port}`));
