e244631d603897a4e2320165ecf65ae97a5a48f2
Guilherme Morone Araujo <51235309+guimorone@users.noreply.github.com>
2024-03-18 09:23:46 -0300

    Reverse 10 second

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -2,4 +2,4 @@ import type { LatLngExpression } from 'leaflet';
 
 export const DEFAULT_DOCUMENT_TITLE: string = import.meta.env.VITE_APP_NAME;
 export const RECIFE_COORDS: LatLngExpression = [-8.05, -34.9];
-export const POLLS_STALE_TIME: number = 0.5 * 1000; // 10 segundos de cache
+export const POLLS_STALE_TIME: number = 10 * 1000; // 10 segundos de cache
